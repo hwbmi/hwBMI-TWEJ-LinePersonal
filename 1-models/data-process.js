0a335383dc48ff95f339a8c122ce0a867f20987c
paulchainkang <paul@paul-kang.com>
2022-07-21 14:58:54 +0800

    twej v1.0

---
+++ 1-models/data-process.js
@@ -0,0 +1,113 @@
+var isAndroid = kendo.support.mobileOS.android;
+
+//var apiSite = (useLocalAPIs)?'http://localhost:5000':'https://hwbmi-api.herokuapp.com/';
+var apiSite = (useLocalAPIs)?'http://localhost:5000':'https://twej-hwbmi.herokuapp.com/';
+console.log(apiSite);
+
+var measurementSource = new kendo.data.DataSource({
+  transport: {
+    read: function (data) { 取得量測記錄(measurementSource); }
+  },
+  sort: {
+    field: "量測紀錄時間",
+    dir: "desc"
+  },
+  requestStart: function () {
+    kendo.ui.progress($("#loading"), true);
+  },
+  requestEnd: function () {
+    kendo.ui.progress($("#loading"), false);
+  },
+
+  schema: {
+    total: function () {
+      console.log("measurementSource scheme total");
+      //取得經緯度();    
+      return 77;
+    }
+  },
+  serverPaging: true,
+  pageSize: 40,
+  //group: { field: "section" }
+})
+
+async function 取得量測記錄(data) {
+  console.log("取得量測記錄");
+  
+  if (!refresh) {  
+    var dataTemp=[];
+    data.success(dataTemp);
+  } else {
+    paramToSend = "?API=33" + "&UserId=" + $("#formHWBMI_ID").val(); 
+    console.log(paramToSend);
+    resStr = await callAPI(paramToSend, '讀取量測記錄');
+
+    var 所有量測數據=JSON.parse(resStr); 
+
+    var dataTemp=[];
+    for (rec in 所有量測數據) {
+      var 時間Date = 所有量測數據[rec].measure_time;
+      console.log("時間Date", 時間Date);      
+      var 卡片 = {
+        "量測記錄時間": 時間Date, //所有量測數據[i].量測時間,              
+        "身高":    所有量測數據[rec].height,
+        "體重":    所有量測數據[rec].net_weight,
+        "BMI":    所有量測數據[rec].bmi,             
+        "url": "2-views/量測報告.html?"+$("#formHWBMI_ID").val()+"&"+rec,
+        "section": "A"             
+      };
+      dataTemp.push(卡片); 
+    }
+    
+    //data.success(dataTemp.reverse());    
+    data.success(dataTemp.sort(function(a,b) {
+        if (a.量測記錄時間 < b.量測記錄時間) return  1;
+        if (a.量測記錄時間 > b.量測記錄時間) return -1;
+        return 0;
+      })
+    );
+    
+    //measurementSource.success(dataTemp);    
+  }
+  
+  if (dataTemp.length==0) {
+    $("#量測記錄title").text("尚無量測記錄");
+  }else {
+    $("#量測記錄title").text("量測記錄");
+  }  
+  //return;
+}
+
+
+function nullForNow(e) {
+  console.log("nullForNow");
+  //currentExample = nullForNow;
+}
+
+function removeView(e) {
+  if (!e.view.element.data("persist")) {
+    //console.log(e);
+    
+    // KPC: 找不到 persist 如何設定，只好用粗暴的做法
+    if (e.view.id != "#forms") e.view.purge();
+    
+    //e.view.purge();
+  }
+
+}
+
+var desktop = !kendo.support.mobileOS;
+
+window.app = new kendo.mobile.Application($(document.body), {
+  layout: "mainDiv",
+  transition: "slide",
+  skin: "nova",
+  icon: {
+    "": '@Url.Content("~/content/mobile/AppIcon72x72.png")',
+    "72x72": '@Url.Content("~/content/mobile/AppIcon72x72.png")',
+    "76x76": '@Url.Content("~/content/mobile/AppIcon76x76.png")',
+    "114x114": '@Url.Content("~/content/mobile/AppIcon72x72@2x.png")',
+    "120x120": '@Url.Content("~/content/mobile/AppIcon76x76@2x.png")',
+    "152x152": '@Url.Content("~/content/mobile/AppIcon76x76@2x.png")'
+  }
+});
---
+++ 3-controllers/functions.js
@@ -0,0 +1,232 @@
+// MM/DD/YYYY ==> YYYY-MM-DD
+function convertDate(dateStr){ 
+  var dateArr = dateStr.split("/");
+  // Kendo UI Calendar 的日期是類似 2/9/2020，月和日不會補0
+  //if (dateArr[0].length ==1) dateArr[0]= "0"+dateArr[0];
+  //if (dateArr[1].length ==1) dateArr[1]= "0"+dateArr[1];
+  return dateArr[2]+"-"+dateArr[0]+"-"+dateArr[1];
+}
+
+// 設定 $a enabled 或 disabled
+function setEnabled($a, Enabled ){
+  $a.each(function(i, a){          
+    var en = a.onclick !== null;        
+    if(en == Enabled)return;
+    if(Enabled){
+      a.onclick = $(a).data('orgClick');            
+    }
+    else
+    {
+      $(a).data('orgClick',a.onclick);
+      a.onclick = null;
+    }
+  });
+}
+
+function 取得經緯度() {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+    目前位置緯度 = Math.floor(position.coords.latitude * 10000) / 10000;
+    目前位置經度 = Math.floor(position.coords.longitude * 10000) / 10000;
+    $("#deleteMe").text("所在位置 緯度: " + String(目前位置緯度) + ", 經度: " + String(目前位置經度));
+  });
+}
+
+// 計算 兩點 間的距離
+function calcDistance(lat1, lon1, lat2, lon2) {
+  var R = 6371000; // meter
+  var dLat = toRad(lat2-lat1);
+  var dLon = toRad(lon2-lon1);
+  var lat1 = toRad(lat1);
+  var lat2 = toRad(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c;
+  return d;
+}
+
+// Converts numeric degrees to radians
+function toRad(Value) 
+{
+    return Value * Math.PI / 180;
+}
+
+
+async function 更新資料() {
+  console.log("更新資料");
+ 
+  await 註冊會員();
+  console.log(已經是會員);
+  
+  app.navigate('#:back');
+}
+
+// 非同步+await
+function callAPI(param, loadingMessage) {
+  return new Promise(function(resolve, reject) {       
+    var request = new XMLHttpRequest();
+    request.open('GET', apiSite +param, true);
+
+    request.onload = function() {
+      if (loadingMessage!="") $.loading.end();
+      //console.log(this.response);
+
+      resolve(this.response);
+    }
+    // Send API request 
+    if (loadingMessage!="") $.loading.start(loadingMessage);
+
+    request.send();    
+  });
+}
+
+async function checkUserIdExist() {
+
+  $.loading.start('檢查是否已註冊');
+  paramToSend = "?API=04" + "&LineId=" + userId[1];
+
+  console.log(apiSite);
+  
+  var res;
+  var resStr = await callAPI(paramToSend, '檢查是否已填寫必要資料'); 
+  $.loading.end();
+  
+  res = JSON.parse(resStr);
+  console.log(res);
+  if (res.error=="NoFund") {
+    console.log("Not Found");
+    alert("為了讓您更容易查詢，請註冊姓名及電話號碼");
+    $("#formUserName").val(decodeURI(displayName[1]));
+    $("#formUserName").attr("disabled", "disabled"); 
+    $("#LINE頭像").attr("src", pictureUrl[1]);
+    $("#formHWBMI_ID").val(res.newId);    
+    已經是會員 = false; 
+    app.navigate('#forms'); 
+    return;
+  } else { // 已經是會員    
+    $("#formUserName").val(decodeURI(displayName[1]));
+    $("#formUserName").attr("disabled", "disabled"); 
+    $("#LINE頭像").attr("src", pictureUrl[1]); 
+    $("#formUserPhone").val(res.電話);
+    $("#formHWBMI_ID").val(res.Id);
+    console.log(res)
+    已經是會員 = true;     
+  }
+      
+  取得量測記錄(measurementSource);
+  refresh=true;
+  measurementSource.read(); // 產生 sorting 
+  //$.loading.end(); 
+  
+  // show barcode
+  var hwBMI_ID=$("#formHWBMI_ID").val();
+  JsBarcode("#barcode", hwBMI_ID, {
+    displayValue: false,
+    width:2,
+    height:70
+  });   
+  
+  return;
+  
+}
+
+async function 註冊會員() {
+  console.log("註冊會員");
+  // 檢查資料格式     
+  if (   $("#formUserName").val()        == ""
+      || $("#formUserPhone").val()       == ""      
+     ) {
+    alert("請填寫必填項目!");
+    
+    return false;
+  }
+
+  var profile = "請確認註冊資料:\n" +
+    "    姓名: " + $("#formUserName").val() + "\n" +                 
+    "    電話: " + $("#formUserPhone").val() + "\n";       
+
+  if (confirm(profile)) {
+
+    // 先綁定 ID, 名字和電話
+    paramToSend = "?API=01" +
+      "&UserId="            + $("#formHWBMI_ID").val()+  
+      "&Name="              + $("#formUserName").val()+  
+      "&Phone="             + $("#formUserPhone").val();  
+  
+    console.log(paramToSend); 
+    
+    var res = await callAPI(paramToSend, '寫入資料');
+
+    if (res == "API:01 會員寫入成功") {
+      //alert("資料更新成功，回到量測頁面");
+      //checkUserIdExist();
+      //已經是會員 = true;
+    } else {
+      alert("資料新增失敗，請重試。若一直有問題，請洽管理員")
+      $("#errorMessage").css("display", "block");
+      return;
+    }
+    
+    // 再綁定 LineID 和 UserID
+    paramToSend = "?API=03" +
+      "&LineId="           + userId[1] +
+      "&UserId="           + $("#formHWBMI_ID").val();  
+
+    console.log(paramToSend); 
+    
+    var res = await callAPI(paramToSend, '寫入資料');
+    
+    if (res == "API:03 會員資料Line綁定hwBMI寫入成功") {
+      alert("資料更新成功，回到量測頁面");
+      checkUserIdExist();
+      已經是會員 = true;
+    } else {
+      alert("資料新增失敗，請重試。若一直有問題，請洽管理員")
+      $("#errorMessage").css("display", "block");
+    }    
+
+  } else {
+    console.log("Cancel");
+  };
+  
+};
+
+function checkInputParam() {
+  //console.log(inputParam);
+  try {
+    displayName = inputParam[0].split("=");
+    userId = inputParam[1].split("=");
+    pictureUrl = inputParam[2].split("=");
+  } catch (e) {
+    inputError = true;
+  }
+
+  //console.log(displayName[1]);
+
+  if (inputError) {
+    alert("輸入參數錯誤");
+    loadCourses = false;
+
+    // 等 #mainDiv 顯示後，再 hide()
+    setTimeout(function(){$("#mainDiv").hide();}, 500);
+
+    $("#errorMessage").css("display", "block"); 
+    return false;
+  }
+    return true;
+}
+
+function 顯示個人資料同意書() {
+  console.log("顯示個人資料同意書");
+  $("#formData").hide();
+  $("#個人資料使用Div").hide();
+  $("#個人資料同意書Div").show();
+}
+
+function 我知道了(){
+  $("#formData").show();
+  $("#個人資料使用Div").show();  
+  $("#個人資料同意書Div").hide();      
+}
